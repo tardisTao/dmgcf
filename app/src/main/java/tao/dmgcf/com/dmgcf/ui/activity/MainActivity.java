1bbd3e62e4da9ffa0312e5294e6d44ed0c6d319d
tardisTao <feng3696@hotmail.com>
2016-01-05 16:29:37 +0800

    添加主体

--- app/src/main/java/tao/dmgcf/com/dmgcf/ui/activity/MainActivity.java
+++ app/src/main/java/tao/dmgcf/com/dmgcf/ui/activity/MainActivity.java
@@ -1,39 +1,88 @@
 package tao.dmgcf.com.dmgcf.ui.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RadioGroup;
 
-import tao.dmgcf.com.dmgcf.AppManager;
+import java.util.Arrays;
+import java.util.List;
+
 import tao.dmgcf.com.dmgcf.R;
+import tao.dmgcf.com.dmgcf.ui.fragment.MoreFragment;
+import tao.dmgcf.com.dmgcf.ui.fragment.MypropertyFragment;
+import tao.dmgcf.com.dmgcf.ui.fragment.ProductFragment;
+import tao.dmgcf.com.dmgcf.ui.fragment.RecommFragment;
 
 /**
  * Created by tao on 2016/1/4.
  */
-public class MainActivity extends  BaseActivity {
-    private Button btn;
-    private Button btn_over;
+public class MainActivity extends BaseActivity {
 
+    private static int currSel=0;
+    private RadioGroup group;
+
+    private Fragment recommFm=new RecommFragment();
+    private Fragment productFm=new ProductFragment();
+    private Fragment mypropertyFm=new MypropertyFragment();
+    private Fragment moreFm=new MoreFragment();
+    private List<Fragment> fragmentList= Arrays.asList(recommFm,productFm,mypropertyFm,moreFm);
+
+    private FragmentManager fragmentManager;
     @Override
     protected void initView(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);
-        btn=getViewById(R.id.btn_main);
-        btn_over=getViewById(R.id.btn_over);
+        setContentView(R.layout.layout_main);
+        fragmentManager=getSupportFragmentManager();
+        group=getViewById(R.id.group_public_footer);
+
+        initFootBar();
     }
 
-    @Override
-    protected void setListener() {
-        btn.setOnClickListener(this);
-        btn_over.setOnClickListener(this);
+    private void initFootBar() {
+
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.rb_foot_bar_recomm:
+                        currSel=0;
+                        break;
+                    case R.id.rb_foot_bar_product:
+                        currSel=1;
+                        break;
+                    case R.id.rb_foot_bar_myproperty:
+                        currSel=2;
+                        break;
+                    case R.id.rb_foot_bar_more:
+                        currSel=3;
+                        break;
+                }
+                addFragmentToStack(currSel);
+                if(currSel == 2){
+                    forward(FirstActivity.class);
+                }
+            }
+        });
+        addFragmentToStack(0);
     }
 
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.btn_main){
-           forward(FirstActivity.class);
-        }else if(view.getId() == R.id.btn_over){
-            AppManager.getInstance().AppExit(this);
+    private void addFragmentToStack(int cur){
+        FragmentTransaction  fragmentTransaction=fragmentManager.beginTransaction();
+        Fragment fragment=fragmentList.get(cur);
+        if(!fragment.isAdded()){
+            fragmentTransaction.add(R.id.fragment_main_container,fragment);
         }
+        for (int i=0;i < fragmentList.size();i++){
+            Fragment f=fragmentList.get(i);
+            if(i == cur && f.isAdded()){
+                fragmentTransaction.show(f);
+            }else if(f != null &&f.isAdded() && f.isVisible()){
+                fragmentTransaction.hide(f);
+            }
+        }
+        fragmentTransaction.commitAllowingStateLoss();
     }
+
 }
---
+++ app/src/main/java/tao/dmgcf/com/dmgcf/ui/fragment/MoreFragment.java
@@ -0,0 +1,9 @@
+package tao.dmgcf.com.dmgcf.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tao on 2016/1/5.
+ */
+public class MoreFragment extends Fragment{
+}
